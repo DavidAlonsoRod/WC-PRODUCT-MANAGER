5d4716703703a54516c7e105e77a93c5133c8d67
Kellbis <holasoykellbis@gmail.com>
2025-03-14 11:50:26 -0400

    navbar improved

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
 import { useNavigate } from "react-router-dom";
@@ -6,80 +6,102 @@ import "../../styles/navbar.css";
 import logo from "../../img/wc_logo.png";
 
 export const Navbar = () => {
-	const navigate = useNavigate();
-	const { store, actions } = useContext(Context);
+  const navigate = useNavigate();
+  const { store, actions } = useContext(Context);
+  const [open, setOpen] = useState(false);
 
-	function handleLogout() {
-		actions.logout();
-		navigate('/');
-		setOpen(false);
+  const handleLogout = () => {
+    actions.logout();
+    navigate('/');
+    setOpen(false);
+  };
 
-	}
+  // Links de navegacion del navbar para usarlos en cualquier momento sin repetir el codigo.
+  const navLinks = (
+    <>
+      <Link to="/orders" onClick={() => setOpen(false)}>
+        <svg className="m-2" xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="24px" fill="#629a97">
+          <path d="M320-280q17 0 28.5-11.5T360-320q0-17-11.5-28.5T320-360q-17 0-28.5 11.5T280-320q0 17 11.5 28.5T320-280Zm0-160q17 0 28.5-11.5T360-480q0-17-11.5-28.5T320-520q-17 0-28.5 11.5T280-480q0 17 11.5 28.5T320-440Zm0-160q17 0 28.5-11.5T360-640q0-17-11.5-28.5T320-680q-17 0-28.5 11.5T280-640q0 17 11.5 28.5T320-600Zm120 320h240v-80H440v80Zm0-160h240v-80H440v80Zm0-160h240v-80H440v80ZM200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Zm0-80h560v-560H200v560Zm0-560v560-560Z" />
+        </svg>
+        Pedidos
+      </Link>
 
-	return (
-		<nav className="navbar border-bottom border-body" id="navbar">
-			<div className="container-fluid">
-				<div className="d-flex col-2 justify-content-center">
-					<img src={logo} alt="Logo" className="navbar-logo ms-3 mx-1" />
-				</div>
+      <Link to="/lineitems" onClick={() => setOpen(false)}>
+        <svg className="m-2" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="20px" fill="#629a97">
+          <path d="M159-120v-120h124L181-574q-27-15-44.5-44T119-680q0-50 35-85t85-35q39 0 69.5 22.5T351-720h128v-40q0-17 11.5-28.5T519-800q9 0 17.5 4t14.5 12l68-64q9-9 21.5-11.5T665-856l156 72q12 6 16.5 17.5T837-744q-6 12-17.5 15.5T797-730l-144-66-94 88v56l94 86 144-66q11-5 23-1t17 15q6 12 1 23t-17 17l-156 74q-12 6-24.5 3.5T619-512l-68-64q-6 6-14.5 11t-17.5 5q-17 0-28.5-11.5T479-600v-40H351q-3 8-6.5 15t-9.5 15l200 370h144v120H159Zm80-520q17 0 28.5-11.5T279-680q0-17-11.5-28.5T239-720q-17 0-28.5 11.5T199-680q0 17 11.5 28.5T239-640Zm126 400h78L271-560h-4l98 320Zm78 0Z" />
+        </svg>
+        Producción
+      </Link>
 
-				{store.auth || localStorage.getItem("token") ? (
-					<div className="col-6 navbar-links d-flex align-items-center">
-						<Link to="/orders">
-							<button className="btn" id="button">
-								<svg className="m-2" xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="24px" fill="#629a97"><path d="M320-280q17 0 28.5-11.5T360-320q0-17-11.5-28.5T320-360q-17 0-28.5 11.5T280-320q0 17 11.5 28.5T320-280Zm0-160q17 0 28.5-11.5T360-480q0-17-11.5-28.5T320-520q-17 0-28.5 11.5T280-480q0 17 11.5 28.5T320-440Zm0-160q17 0 28.5-11.5T360-640q0-17-11.5-28.5T320-680q-17 0-28.5 11.5T280-640q0 17 11.5 28.5T320-600Zm120 320h240v-80H440v80Zm0-160h240v-80H440v80Zm0-160h240v-80H440v80ZM200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Zm0-80h560v-560H200v560Zm0-560v560-560Z" /></svg>
-								Pedidos
-							</button>
-						</Link>
-						{/* <Link to="/invoices">
-							<button className="btn" id="button">
-								<svg className="m-2" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="20px" fill="#629a97"><path d="M240-80q-50 0-85-35t-35-85v-120h120v-560l60 60 60-60 60 60 60-60 60 60 60-60 60 60 60-60 60 60 60-60v680q0 50-35 85t-85 35H240Zm480-80q17 0 28.5-11.5T760-200v-560H320v440h360v120q0 17 11.5 28.5T720-160ZM360-600v-80h240v80H360Zm0 120v-80h240v80H360Zm320-120q-17 0-28.5-11.5T640-640q0-17 11.5-28.5T680-680q17 0 28.5 11.5T720-640q0 17-11.5 28.5T680-600Zm0 120q-17 0-28.5-11.5T640-520q0-17 11.5-28.5T680-560q17 0 28.5 11.5T720-520q0 17-11.5 28.5T680-480ZM240-160h360v-80H200v40q0 17 11.5 28.5T240-160Zm-40 0v-80 80Z" /></svg>
-								Facturas
-							</button>
-						</Link> */}
-						<Link to="/lineitems">
-							<button className="btn" id="button">
-								<svg className="m-2" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="20px" fill="#629a97"><path d="M159-120v-120h124L181-574q-27-15-44.5-44T119-680q0-50 35-85t85-35q39 0 69.5 22.5T351-720h128v-40q0-17 11.5-28.5T519-800q9 0 17.5 4t14.5 12l68-64q9-9 21.5-11.5T665-856l156 72q12 6 16.5 17.5T837-744q-6 12-17.5 15.5T797-730l-144-66-94 88v56l94 86 144-66q11-5 23-1t17 15q6 12 1 23t-17 17l-156 74q-12 6-24.5 3.5T619-512l-68-64q-6 6-14.5 11t-17.5 5q-17 0-28.5-11.5T479-600v-40H351q-3 8-6.5 15t-9.5 15l200 370h144v120H159Zm80-520q17 0 28.5-11.5T279-680q0-17-11.5-28.5T239-720q-17 0-28.5 11.5T199-680q0 17 11.5 28.5T239-640Zm126 400h78L271-560h-4l98 320Zm78 0Z" /></svg>
-								Producción
-							</button>
-						</Link>
+      <Link to="/scanqr" onClick={() => setOpen(false)}>
+        <svg className="m-2" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="20px" fill="#629a97">
+          <path d="M480-80q-150 0-255-105T120-440q0-150 105-255t255-105q150 0 255 105t105 255q0 150-105 255T480-80Zm0-60q125 0 212.5-87.5T780-440q0-125-87.5-212.5T480-740q-125 0-212.5 87.5T180-440q0 125 87.5 212.5T480-140Zm-40-160h80v-80h-80v80Zm0-160h80v-240h-80v240Z" />
+        </svg>
+        Escanear QR
+      </Link>
 
-						<Link to="/scanqr">
-							<button className="btn" id="button">
-								<svg className="m-2" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="20px" fill="#629a97"><path d="M480-80q-150 0-255-105T120-440q0-150 105-255t255-105q150 0 255 105t105 255q0 150-105 255T480-80Zm0-60q125 0 212.5-87.5T780-440q0-125-87.5-212.5T480-740q-125 0-212.5 87.5T180-440q0 125 87.5 212.5T480-140Zm-40-160h80v-80h-80v80Zm0-160h80v-240h-80v240Z" /></svg>
-								Escanear QR
-							</button>
-						</Link>
-						<Link to="/customers">
-							<button className="btn" id="button">
-								<svg className="m-2" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="20px" fill="#629a97"><path d="M40-160v-112q0-34 17.5-62.5T104-378q62-31 126-46.5T360-440q66 0 130 15.5T616-378q29 15 46.5 43.5T680-272v112H40Zm720 0v-120q0-44-24.5-84.5T666-434q51 6 96 20.5t84 35.5q36 20 55 44.5t19 53.5v120H760ZM360-480q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47Zm400-160q0 66-47 113t-113 47q-11 0-28-2.5t-28-5.5q27-32 41.5-71t14.5-81q0-42-14.5-81T544-792q14-5 28-6.5t28-1.5q66 0 113 47t47 113ZM120-240h480v-32q0-11-5.5-20T580-306q-54-27-109-40.5T360-360q-56 0-111 13.5T140-306q-9 5-14.5 14t-5.5 20v32Zm240-320q33 0 56.5-23.5T440-640q0-33-23.5-56.5T360-720q-33 0-56.5 23.5T280-640q0 33 23.5 56.5T360-560Zm0 320Zm0-400Z" /></svg>
-								Clientes
-							</button>
-						</Link>
+      <Link to="/customers" onClick={() => setOpen(false)}>
+        <svg className="m-2" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="20px" fill="#629a97">
+          <path d="M40-160v-112q0-34 17.5-62.5T104-378q62-31 126-46.5T360-440q66 0 130 15.5T616-378q29 15 46.5 43.5T680-272v112H40Zm720 0v-120q0-44-24.5-84.5T666-434q51 6 96 20.5t84 35.5q36 20 55 44.5t19 53.5v120H760ZM360-480q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47Zm400-160q0 66-47 113t-113 47q-11 0-28-2.5t-28-5.5q27-32 41.5-71t14.5-81q0-42-14.5-81T544-792q14-5 28-6.5t28-1.5q66 0 113 47t47 113ZM120-240h480v-32q0-11-5.5-20T580-306q-54-27-109-40.5T360-360q-56 0-111 13.5T140-306q-9 5-14.5 14t-5.5 20v32Zm240-320q33 0 56.5-23.5T440-640q0-33-23.5-56.5T360-720q-33 0-56.5 23.5T280-640q0 33 23.5 56.5T360-560Zm0 320Zm0-400Z" />
+        </svg>
+        Clientes
+      </Link>
 
-						<Link to="/settings">
-							<button className="btn" id="button">
-								<svg className="m-2" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#629a97"><path d="m234-480-12-60q-12-5-22.5-10.5T178-564l-58 18-40-68 46-40q-2-13-2-26t2-26l-46-40 40-68 58 18q11-8 21.5-13.5T222-820l12-60h80l12 60q12 5 22.5 10.5T370-796l58-18 40 68-46 40q2 13 2 26t-2 26l46 40-40 68-58-18q-11 8-21.5 13.5T326-540l-12 60h-80Zm40-120q33 0 56.5-23.5T354-680q0-33-23.5-56.5T274-760q-33 0-56.5 23.5T194-680q0 33 23.5 56.5T274-600ZM592-40l-18-84q-17-6-31.5-14.5T514-158l-80 26-56-96 64-56q-2-18-2-36t2-36l-64-56 56-96 80 26q14-11 28.5-19.5T574-516l18-84h112l18 84q17 6 31.5 14.5T782-482l80-26 56 96-64 56q2 18 2 36t-2 36l64 56-56 96-80-26q-14 11-28.5 19.5T722-124l-18 84H592Zm56-160q50 0 85-35t35-85q0-50-35-85t-85-35q-50 0-85 35t-35 85q0 50 35 85t85 35Z" /></svg>
-								Configuración
-							</button>
-						</Link>
-						<div className="ms-auto">
-							<Link to="/">
-								<button onClick={handleLogout} className="btn" id="button">
-									<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="red"><path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h280v80H200Zm440-160-55-58 102-102H360v-80h327L585-622l55-58 200 200-200 200Z" /></svg>
-									Salir
-								</button>
-							</Link>
-						</div>
-					</div>
-				) : (
-					<div className="col-4 navbar-titles">
-						<h2 className="m-1">Woocomerce Product Manager</h2>
-						<h5 className="m-1">Gestión de fabricación y envío de productos de Woocommerce</h5>
-					</div>
-				)
-				}
-			</div >
-		</nav >
-	);
+      <Link to="/settings" onClick={() => setOpen(false)}>
+        <svg className="m-2" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#629a97">
+          <path d="m234-480-12-60q-12-5-22.5-10.5T178-564l-58 18-40-68 46-40q-2-13-2-26t2-26l-46-40 40-68 58 18q11-8 21.5-13.5T222-820l12-60h80l12 60q12 5 22.5 10.5T370-796l58-18 40 68-46 40q2 13 2 26t-2 26l46 40-40 68-58-18q-11 8-21.5 13.5T326-540l-12 60h-80Zm40-120q33 0 56.5-23.5T354-680q0-33-23.5-56.5T274-760q-33 0-56.5 23.5T194-680q0 33 23.5 56.5T274-600ZM592-40l-18-84q-17-6-31.5-14.5T514-158l-80 26-56-96 64-56q-2-18-2-36t2-36l-64-56 56-96 80 26q14-11 28.5-19.5T574-516l18-84h112l18 84q17 6 31.5 14.5T782-482l80-26 56 96-64 56q2 18 2 36t-2 36l64 56-56 96-80-26q-14 11-28.5 19.5T722-124l-18 84H592Zm56-160q50 0 85-35t35-85q0-50-35-85t-85-35q-50 0-85 35t-35 85q0 50 35 85t85 35Z" />
+        </svg>
+        Configuración
+      </Link>
+
+      <Link to="/" onClick={handleLogout}>
+        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="red">
+          <path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h280v80H200Zm440-160-55-58 102-102H360v-80h327L585-622l55-58 200 200-200 200Z" />
+        </svg>
+        Salir
+      </Link>
+    </>
+  );
+
+  return (
+    <nav className="navbar" id="navbar">
+      <div className="container-fluid">
+        <div className="logo-container">
+          <img src={logo} alt="Logo" className="navbar-logo" />
+        </div>
+
+        {store.auth || localStorage.getItem("token") ? (
+          <>
+            {/* Desktop */}
+            <div className="navbar__content__right__desktop">
+              {navLinks}
+            </div>
+
+            {/* Mobile menu button don't delete */}
+            <button
+              className="menu-bars-button"
+              aria-label="Abrir menú"
+              aria-expanded={open}
+              onClick={() => setOpen(!open)}
+            >
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" width="24px" className="bars-icon_mobile">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5" />
+              </svg>
+            </button>
+          </>
+        ) : (
+          <div className="navbar-title">
+            <h2>Woocomerce Product Manager</h2>
+            <p>Gestión de fabricación y envío de productos de Woocommerce</p>
+          </div>
+        )}
+      </div>
+      {/* Navigation panel for mobile media */}
+      {(store.auth || localStorage.getItem("token")) && open && (
+        <div className="navbar__content__right__mobile">
+          {navLinks}
+        </div>
+      )}
+    </nav>
+  );
 };
